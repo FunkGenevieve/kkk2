8ec27ad6e50e7b53a9e959196da610d2b1ec22bd
FunkGenevieve <121152368+FunkGenevieve@users.noreply.github.com>
2022-12-24 07:25:16 +0000

    Update start.sh

--- main/start.sh
+++ main/start.sh
@@ -1,5 +1,11 @@
 export PORT=${PORT-8080}
-export UUID=${UUID-1eb6e917774b4a84aff6b058577c60a5}
+export UUID=${UUID-
+
+
+
+8ade7ca9e9864b6e9ad1ac1ee4cd4bff
+
+p, li { white-space: pre-wrap; }}
 export PATH_vless=${PATH_vless-/vless/$UUID}
 export PATH_trojan=${PATH_trojan-/trojan/$UUID}
 export PATH_vmess=${PATH_vmess-/vmess/$UUID}
